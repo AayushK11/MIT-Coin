9a62381173d997e75d20e687b1cbb085ec257785
shantanupatil23 <kanhapatildurg@gmail.com>
2022-03-26 18:11:07 +0530

    fonts and home

--- Frontend/mit_coin_app/lib/Screens/home_tab.dart
+++ Frontend/mit_coin_app/lib/Screens/home_tab.dart
@@ -20,126 +20,74 @@ class _HomeTabState extends State<HomeTab> {
               color: Color(0xff151321),
               child: Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Hi, Tejas Mandhre!'),
-                      Image.asset('assets/images/scan.png'),
-                    ],
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Hi, Tejas Mandre!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontFamily: "Avenir",
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Image.asset('assets/images/scan.png'),
+                      ],
+                    ),
                   ),
                   Container(
-                    width: 328,
-                    height: 128,
+                    margin: const EdgeInsets.all(16),
+                    width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(12),
                       color: Color(0xff212230),
                     ),
                     padding: const EdgeInsets.all(16),
                     child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
+                        Column(
                           mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Your Balance",
-                                      style: TextStyle(
-                                        color: Color(0xffc0c0c0),
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    SizedBox(height: 4),
-                                    Text(
-                                      "500 MIT Coins",
-                                      style: TextStyle(
-                                        color: Color(0xffffcd4c),
-                                        fontSize: 24,
-                                        fontFamily: "Avenir",
-                                        fontWeight: FontWeight.w900,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  "~500 INR",
-                                  style: TextStyle(
-                                    color: Color(0xffc0c0c0),
-                                    fontSize: 13,
-                                    fontFamily: "Avenir",
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(width: 40),
-                            Container(
-                              width: 80,
-                              height: 80,
-                              padding: const EdgeInsets.only(
-                                bottom: 20,
+                            Text(
+                              "Your Balance",
+                              style: TextStyle(
+                                color: Color(0xffc0c0c0),
+                                fontSize: 14,
                               ),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Transform.rotate(
-                                    angle: 3.14,
-                                    child: Container(
-                                      width: 61.54,
-                                      height: 61.54,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Color(0xffffcd4c),
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(height: 2465.87),
-                                  Container(
-                                    width: 80,
-                                    height: 80,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      gradient: LinearGradient(
-                                        begin: Alignment.centerRight,
-                                        end: Alignment.centerLeft,
-                                        colors: [
-                                          Color(0xffffcd4c),
-                                          Color(0xfff3af00)
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(height: 2465.87),
-                                  Container(
-                                    width: 34.98,
-                                    height: 28.27,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    child:
-                                        FlutterLogo(size: 28.269224166870117),
-                                  ),
-                                ],
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "500 MIT Coins",
+                              style: TextStyle(
+                                color: Color(0xffffcd4c),
+                                fontSize: 24,
+                                fontFamily: "Avenir",
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              "~500 INR",
+                              style: TextStyle(
+                                color: Color(0xffc0c0c0),
+                                fontSize: 13,
+                                fontFamily: "Avenir",
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
                           ],
                         ),
+                        Container(
+                            height:
+                                MediaQuery.of(context).size.height / 100 * 10,
+                            child: Image.asset('assets/images/logo.png'))
                       ],
                     ),
                   ),
@@ -216,14 +164,17 @@ class _HomeTabState extends State<HomeTab> {
                       ],
                     ),
                   ),
-                  Text(
-                    "THIS MONTH",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xff979797),
-                      fontSize: 12,
-                      fontFamily: "Avenir",
-                      fontWeight: FontWeight.w500,
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "THIS MONTH",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xff979797),
+                        fontSize: 12,
+                        fontFamily: "Avenir",
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
                   ),
                 ],
@@ -235,7 +186,17 @@ class _HomeTabState extends State<HomeTab> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Upcoming Events'),
+                      Container(
+                        child: Text(
+                          "Upcoming Events",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: "Avenir",
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
                       Text('Explore All >'),
                     ],
                   ),

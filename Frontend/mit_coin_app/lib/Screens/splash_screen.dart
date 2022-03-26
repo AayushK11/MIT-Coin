26a1bf70016d9acc9dcd606fc954a86f38ab943c
shantanupatil23 <kanhapatildurg@gmail.com>
2022-03-27 03:24:22 +0530

    splash fix

--- Frontend/mit_coin_app/lib/Screens/splash_screen.dart
+++ Frontend/mit_coin_app/lib/Screens/splash_screen.dart
@@ -20,8 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    startTimer();
-    makePostRequest();
+    loginCheck();
     // makeGetRequest();
   }
 

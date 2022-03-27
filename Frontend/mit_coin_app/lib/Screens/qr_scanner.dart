521c8005aa0f58e0f15300c186ab4ba445423e41
shantanupatil23 <kanhapatildurg@gmail.com>
2022-03-27 07:14:51 +0530

    qr

--- Frontend/mit_coin_app/lib/Screens/qr_scanner.dart
+++ Frontend/mit_coin_app/lib/Screens/qr_scanner.dart
@@ -2,7 +2,7 @@ import 'package:mit_coin_app/Screens/checkout_amount_page.dart';
 import 'package:mit_coin_app/Screens/checkout_page.dart';
 import 'package:mit_coin_app/Screens/checkout_security_page.dart';
 import 'package:page_transition/page_transition.dart';
-// import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'dart:developer';
 import 'dart:io';
 
@@ -35,83 +35,83 @@ class QRViewExample extends StatefulWidget {
 }
 
 class _QRViewExampleState extends State<QRViewExample> {
-  // Barcode? result;
-  // QRViewController? controller;
+  Barcode? result;
+  QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
-  // @override
-  // void reassemble() {
-  //   super.reassemble();
-  //   if (Platform.isAndroid) {
-  //     controller!.pauseCamera();
-  //   }
-  //   controller!.resumeCamera();
-  // }
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          // Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(flex: 4, child: _buildQrView(context)),
         ],
       ),
     );
-    // }
+  }
 
-    // Widget _buildQrView(BuildContext context) {
-    //   // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    //   var scanArea = (MediaQuery.of(context).size.width < 400 ||
-    //           MediaQuery.of(context).size.height < 400)
-    //       ? 150.0
-    //       : 300.0;
-    //   // To ensure the Scanner view is properly sizes after rotation
-    //   // we need to listen for Flutter SizeChanged notification and update controller
-    //   return QRView(
-    //     key: qrKey,
-    //     onQRViewCreated: _onQRViewCreated,
-    //     overlay: QrScannerOverlayShape(
-    //         borderColor: Colors.red,
-    //         borderRadius: 10,
-    //         borderLength: 30,
-    //         borderWidth: 10,
-    //         cutOutSize: scanArea),
-    //     onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-    //   );
-    // }
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
 
-    // void _onQRViewCreated(QRViewController controller) {
-    //   setState(() {
-    //     this.controller = controller;
-    //   });
-    //   controller.scannedDataStream.listen((scanData) {
-    //     setState(() {
-    //       result = scanData;
-    //     });
-    //     Navigator.push(
-    //       context,
-    //       PageTransition(
-    //           duration: Duration(milliseconds: 250),
-    //           type: PageTransitionType.rightToLeft,
-    //           child: CheckoutAmountScreen()),
-    //     );
-    //   });
-    // }
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      Navigator.push(
+        context,
+        PageTransition(
+            duration: Duration(milliseconds: 250),
+            type: PageTransitionType.rightToLeft,
+            child: CheckoutAmountScreen()),
+      );
+    });
+  }
 
-    // void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    //   log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-    //   if (!p) {
-    //     ScaffoldMessenger.of(context).showSnackBar(
-    //       const SnackBar(content: Text('no Permission')),
-    //     );
-    //   }
-    // }
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
 
-    // @override
-    // void dispose() {
-    //   controller?.dispose();
-    //   super.dispose();
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
